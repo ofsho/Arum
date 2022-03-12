8abde3c4e530c914fabafede3858ef8ca0a08b4e
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-03-12 22:52:40 +0800

    move lexer to parser

---
+++ src/modules/lexer.ts
@@ -0,0 +1,32 @@
+import { compile } from "moo";
+
+export let lexer = compile({
+    // Whitespace & comments
+    ws: /[ \t]+/,
+    comment: /\/\/.*/,
+
+    // Types
+	type: ['identifier', 'string', 'int', 'boolean', 'null', 'undefined'],
+    identifier: /[a-zA-Z_][a-zA-Z0-9_]*/,
+    number: /[0-9]+/,
+    string: /"(?:\\["\\]|[^\n"\\])*"/,
+    boolean: /true|false/,
+    null: /null/,
+    undefined: /undefined/,
+
+    // Keywords
+    var_type: ['set', 'local', 'const',],
+    break: 'break',
+
+    // Other stuff....
+    lparen: '(',
+    rparen: ')',
+    lbracket: '[',
+    rbracket: ']',
+    lbrace: '{',
+    rbrace: '}',
+    longarrow: '=>',
+    colon: ":",
+    assign: '=',
+    newline: { match: /[\n\r]+/, lineBreaks: true },
+});
